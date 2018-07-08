5f931b1c68a8f609eeff91f47ea877cde06b98e0
Darin <dario.princ@gmail.com>
2018-07-08 21:46:46 +0200

    append testing ready

--- test/appendTest.js
+++ test/appendTest.js
@@ -1,27 +1,31 @@
+const fs = require('fs');
 const fsMock = require('mock-fs');
 const assert = require('chai').assert;
+
+const appendComponent = require('../src/append');
 
 const dataToAppend = {
     name : "Dave",
     count : 4
 };
 
-const fs = require('fs');
-const appendComponent = require('../src/append');
-fsMock({
-    'test': {
-        'testData.txt': ''
-    }
-});
+const stringifiedDataWithSeparator = JSON.stringify(dataToAppend) + appendComponent.separator;
 
-const stringifiedDataWithSeparator = JSON.stringify(dataToAppend + appendComponent.separator);
+beforeEach(() => {
+    fsMock({
+        'test': {
+            'testData.txt': ''
+        }
+    });
+});
+afterEach(fsMock.restore);
 
 describe('Append', () => {
     describe('appendData()', () => {
         it('appendData should take a stringyfied dataObject and append it to the file', () => {
             appendComponent.appendData(dataToAppend, './test/testData.txt');
             const appendedData = fs.readFileSync('./test/testData.txt', 'utf8');
-            assert.equal(stringifiedDataWithSeparator, appendedData);
+            assert.equal(appendedData, stringifiedDataWithSeparator);
         });
     });
 });
